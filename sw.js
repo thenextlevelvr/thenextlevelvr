4dd5a6a1f997528318a49cbac771f1425668ead0
Marti <44981000+marti-toonen@users.noreply.github.com>
2023-02-25 18:58:10 -0500

    Trying To Do a PWA

---
+++ sw.js
@@ -0,0 +1,10 @@
+var github-path = '/thenextlevelvr';
+var app-prefix = 'tnlvr';
+
+var version = 'version_00';
+
+var urls = [ 
+	`${github-path}/`, 
+	`${github-path}/index.html`, 
+	`${github-path}/css/styles.css`, 
+	`${github-path}/js/app.js` ]
